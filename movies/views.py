726b0a9d392dded91a423df9c7f8a08cca9fe63e
ParkSanggi <roqkfwkehlwk@naver.com>
2019-07-05 21:39:56 +0900

    "CreateList View 수정 및 작동확인"

--- movies/views.py
+++ movies/views.py
@@ -27,13 +27,12 @@ class CreateLike(View):
 
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            if 'movie_id' in kwargs['movie_id']:
-                parent_user = request.user
-                sub_user = parent_user.sub_user.all().filter(logined=True).get()
-                movie = Movie.objects.get(pk=request.kwargs['movie_id'])
-                if sub_user in movie.likes.all():
-                    movie.likes.remove(sub_user)
-                else:
-                    movie.likes.add(sub_user)
-
-        return JsonResponse("찜 목록에 추가되었습니다.")
+            parent_user = request.user
+            sub_user = parent_user.sub_user.all().filter(logined=True).get()
+            movie = Movie.objects.get(pk=kwargs['movie_id'])
+            if sub_user in movie.likes.all():
+                movie.likes.remove(sub_user)
+                return JsonResponse({'data': 'remove'})
+            else:
+                movie.likes.add(sub_user)
+                return JsonResponse({'data': 'add'})
