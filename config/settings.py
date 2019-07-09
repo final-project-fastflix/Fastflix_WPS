3177560efc867f021ad057f44386d861f04297dc
sug5806 <sug5806@gmail.com>
2019-07-09 13:16:00 +0900

    feat: whitenoise 미들웨어 위치 조정

--- config/settings.py
+++ config/settings.py
@@ -54,13 +54,14 @@ HAYSTACK_CONNECTIONS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
