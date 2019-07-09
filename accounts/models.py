59f7419cc266b4e26edc73619ca67479589174ac
ParkSanggi <roqkfwkehlwk@naver.com>
2019-07-09 10:55:54 +0900

    "movie 모델에 이미지필드 3개 추가(horizonta, vertical, circle),로고필드, 조회수필드, 추가"

--- accounts/models.py
+++ accounts/models.py
@@ -21,9 +21,11 @@ class User(AbstractUser):
 
 
 class SubUser(models.Model):
+
     name = models.CharField(max_length=20)
     parent_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='sub_user')
     like = models.ManyToManyField(Movie, related_name='likes')
+    dislike = models.ManyToManyField(Movie, related_name='dislikes')
     kid = models.BooleanField(default=False)
     logined = models.BooleanField(default=False)
 
--- movies/models.py
+++ movies/models.py
@@ -47,7 +47,7 @@ class Degree(models.Model):
 
 class Movie(models.Model):
     name = models.CharField(max_length=50)
-    video_file = models.FileField(upload_to='media/%Y/%m/%d')
+    video_file = models.FileField(upload_to=f'media/movie/{name}/video')
     directors = models.ManyToManyField(Director, related_name='movie_directors')
     actors = models.ManyToManyField(Actor, related_name='movie_actors')
     feature = models.ManyToManyField(Feature, related_name='movie_feature')
@@ -58,6 +58,21 @@ class Movie(models.Model):
     uploaded = models.DateField(default=timezone.now)
     synopsis = models.TextField()
     running_time = models.CharField(max_length=10)
+    view_count = models.PositiveIntegerField(blank=True)
+    logo = models.ImageField(upload_to=f'media/movie/{name}/logo')
+    horizontal_photo = models.ImageField(upload_to=f'media/movie/{name}/horizontal')
+    vertical_photo = models.ImageField(upload_to=f'media/movie/{name}/horizontal')
+    circle_photo = models.ImageField(upload_to=f'media/movie/{name}/circle')
+
+    # 영화 국적
+    # 이미지필드 최소 3개
+    # 세로 영
+    # 미리보기 동영상(마우스 오버를 하면 요청을 받아 보내기)
+    # 이미지 가로,세로요청을 헤더로 T/F를 넣어 보냄
+    # 영화 이어보기(멈췄던) 시간
+
+    # 절찬 스트리밍?
+
 
     def __str__(self):
         return self.name
