0919af848e7f333238f23d4249ebf4f54bd84120
ParkSanggi <roqkfwkehlwk@naver.com>
2019-07-16 15:05:41 +0900

    feat: movie detail 뷰 추가

--- movies/models.py
+++ movies/models.py
@@ -54,7 +54,7 @@ class Movie(models.Model):
     directors = models.ManyToManyField(Director, related_name='movie')
     actors = models.ManyToManyField(Actor, related_name='movie')
     feature = models.ManyToManyField(Feature, related_name='movie')
-    author = models.ManyToManyField(Author, related_name='movie')
+    author = models.ManyToManyField(Author, related_name='movie', blank=True)
     degree = models.ForeignKey(Degree, on_delete=models.SET_NULL, related_name='movie_degree', null=True)
     genre = models.ManyToManyField(Genre, related_name='movie')
 
@@ -65,11 +65,11 @@ class Movie(models.Model):
     running_time = models.CharField(max_length=10)
     view_count = models.PositiveIntegerField(default=0)
 
-    logo_image_path = models.TextField(default="")
-    horizontal_image_path = models.TextField(default="")
+    logo_image_path = models.TextField(default=None)
+    horizontal_image_path = models.TextField(default=None)
     vertical_image = models.ImageField(upload_to=f'media/movie/{name}/horizontal')
     circle_image = models.ImageField(upload_to=f'media/movie/{name}/circle')
-    big_image_path = models.TextField(default="")
+    big_image_path = models.TextField(default=None)
 
     def __str__(self):
         return self.name
@@ -78,7 +78,7 @@ class Movie(models.Model):
 class MovieContinue(models.Model):
     movie_id = models.ForeignKey(Movie, related_name='movie_continue', on_delete=models.CASCADE)
     sub_user_id = models.ForeignKey('accounts.SubUser', related_name='movie_continue', on_delete=models.CASCADE)
-    to_be_continue = models.CharField(max_length=10)
+    to_be_continue = models.CharField(max_length=20)
 
     def __str__(self):
         return str(self.movie_id) + " " + str(self.sub_user_id) + " " + str(self.to_be_continue)
